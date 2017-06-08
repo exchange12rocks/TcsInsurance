beaa0affb39b75c6b21b9069ee38a857d2c40f7f
iamainow <iamainow@gmail.com>
2017-06-08 19:53:24 +0400

    + db.Settings

--- TcsInsurance/TcsInsurance/Entities/Model.cs
+++ TcsInsurance/TcsInsurance/Entities/Model.cs
@@ -1,16 +1,20 @@
 namespace TcsInsurance.Entities
 {
+    using System;
     using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
 
     public partial class Model : DbContext
     {
         public Model()
-            : base("name=TinkoffInsuranceModel")
+            : base("name=Model")
         {
         }
 
         public virtual DbSet<Log> Logs { get; set; }
         public virtual DbSet<TickerHistoryValue> TickerHistoryValues { get; set; }
+        public virtual DbSet<Setting> Settings { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
